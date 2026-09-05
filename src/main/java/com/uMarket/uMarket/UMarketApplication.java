045ec282b1bd533a8ff4bd6dcd8a80e107ffb63a
yonkyyyyyy <camachoguerrafardhy2005@gmail.com>
2026-09-04 22:27:52 -0500

    Inicializacion con Spring Boot

---
+++ src/main/java/com/uMarket/uMarket/UMarketApplication.java
@@ -0,0 +1,13 @@
+package com.uMarket.uMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uMarket/uMarket/UMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uMarket.uMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
