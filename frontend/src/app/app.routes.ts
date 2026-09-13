8f66618f18ddc7ec46498a67e644c104d770230b
david adrian socola cisneros <davidadriansocola15@gmail.com>
2026-09-13 16:47:42 -0500

    Pagina publicaciones

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Publicaciones } from './pages/publicaciones/publicaciones';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+  {
+    path: 'publicaciones',
+    component: Publicaciones
   },
   {
     path: '',
---
+++ frontend/src/app/pages/publicaciones/publicaciones.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-publicaciones',
+  styleUrl: './publicaciones.css',
+  templateUrl: './publicaciones.html',
+})
+export class Publicaciones {}
