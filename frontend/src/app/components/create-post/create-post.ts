257f7e7027b24261515eb8c1b0ada8ff2f79aa7a
david adrian socola cisneros <davidadriansocola15@gmail.com>
2026-09-13 16:50:57 -0500

    Componente de publicaciones pe

---
+++ frontend/src/app/components/create-post/create-post.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-create-post',
+  styleUrl: './create-post.css',
+  templateUrl: './create-post.html',
+})
+export class CreatePost {}
