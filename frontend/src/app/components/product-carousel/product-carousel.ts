b9b4e360f7eedd3c37b5e28cd2982f8b86361e41
david adrian socola cisneros <davidadriansocola15@gmail.com>
2026-09-13 16:41:38 -0500

    Componente carrusel para productos destacados

---
+++ frontend/src/app/components/product-carousel/product-carousel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-product-carousel',
+  styleUrl: './product-carousel.css',
+  templateUrl: './product-carousel.html',
+})
+export class ProductCarousel {}
